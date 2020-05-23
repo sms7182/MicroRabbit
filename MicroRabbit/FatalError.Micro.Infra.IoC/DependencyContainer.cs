13c324ee4946a96fd54833e8aae55540c648c955
sms7182 <sms7182@gmail.com>
2020-05-23 10:53:09 +0430

    initial commit infrastructer dependenct ioc

---
+++ MicroRabbit/FatalError.Micro.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,17 @@
+﻿using FatalError.Micro.Core.Bus;
+using MicroRabbit.Infra.Bus;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FatalError.Micro.Infra.IoC
+{
+    public class DependencyContainer
+    { 
+        public static void RegisterServices(IServiceCollection services)
+        {
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
